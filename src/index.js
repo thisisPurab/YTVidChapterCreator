b97910c94c3576923a6bd7f65998242181147b31
atmossnowy <purabrtamboli@gmail.com>
2024-11-19 11:31:49 +0530

    persistor added

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./redux/userSLice";
-import playerReducer from "./redux/playerSlice";
 import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
-
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-        player: playerReducer,
-    },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate
+                loading={null}
+                persistor={persistor}
+            >
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
